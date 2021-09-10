cefa35b7e64f20a46dfa88437992682a2fbc9c98
moeka kitamura <moeka.k76@gmail.com>
2021-09-10 17:03:48 +0900

    add blogs route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
       mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
     end
   post "/graphql", to: "graphql#execute"
-  
+  resources :blogs
 end
